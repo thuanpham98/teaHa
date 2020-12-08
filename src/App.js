ce55fa835f5e068e77590879dc29f47a15560db1
thuan <pmttmp24@gmail.com>
2020-12-08 11:19:24 +0700

    h

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+
+import MainLayout from './containers/home';
+
+function App() {
+  return (
+    <div className="App">
+      <MainLayout>
+
+
+      </MainLayout>
+    </div>
+  );
+}
+
+export default App;
